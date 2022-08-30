b20c56f0e36a6aa02f8d93fca05e8b0f02f4dda3
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-08-30 19:23:35 +0300

    implemented state persistance

--- task-manager/src/main.jsx
+++ task-manager/src/main.jsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {Provider} from 'react-redux';
-import {store} from './redux-store/globalStore';
+//import {store} from './redux-store/globalStore';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux-store/globalStore';
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
--- task-manager/src/redux-store/globalStore.js
+++ task-manager/src/redux-store/globalStore.js
@@ -1,13 +1,42 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, compose} from 'redux';
 import todoReducer from './reducers/TodoReducer';
 
-let allReducers = combineReducers({
-    todo: todoReducer,
-})
+import storage from 'redux-persist/lib/storage';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+import {configureStore} from '@reduxjs/toolkit';
 
 
-export const store = createStore(
-    allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+// let allReducers = combineReducers({
+//     todo: todoReducer,
+// })
 
+// export const store = createStore(
+//     allReducers,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+
+const persistConfig = {
+    key: 'todo',
+    storage,
+};
+const reducers = combineReducers({ todo: todoReducer });
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
